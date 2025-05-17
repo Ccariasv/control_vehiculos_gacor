a3472b7b8611b85014ca88b04e4d618f2589c804
Carlos Carias <carloscariasvidal@gmail.com>
2025-05-16 22:34:32 -0600

    inicio de proyecto, control vehiculos Gacor

---
+++ src/main/java/com/gacor/control_vehiculos_gacor/ControlVehiculosGacorApplication.java
@@ -0,0 +1,13 @@
+package com.gacor.control_vehiculos_gacor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControlVehiculosGacorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ControlVehiculosGacorApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/gacor/control_vehiculos_gacor/ControlVehiculosGacorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gacor.control_vehiculos_gacor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControlVehiculosGacorApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
